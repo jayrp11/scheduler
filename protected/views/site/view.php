f674645ae4c79ddc1cbae929319fe934feac0298
Jaydeep Patel <nisharg@gmail.com>
2014-01-02 11:49:29 +1100

    ui changes for view

--- protected/views/site/view.php
+++ protected/views/site/view.php
@@ -1,6 +1,6 @@
 <div class="view">
 	<h1><?php echo CHtml::encode($model->theme) ?></h3>
-	<h2><?php echo CHtml::encode($model->s_date) ?></h2>
+	<h6><?php echo date('d-m-Y', strtotime($model->s_date)) ?></h6>
 
 	<?php 
 		foreach($model->sub_schedules as $subs)
@@ -8,11 +8,20 @@
 	?>
 			<h3><?php echo CHtml::encode($subs->title) ?></h3>
 			<div>
-				<span><?php echo CHtml::encode($subs->start_time) ?></span> to <span><?php echo CHtml::encode($subs->end_time) ?></span>
+				<span><?php echo date('H:i', strtotime($subs->start_time)) ?></span> to <span><?php echo date('H:i', strtotime($subs->end_time)) ?></span>
 			</div>
 			
 			<div><?php echo CHtml::encode($subs->presenter) ?></div>
 			<div><?php echo CHtml::encode($subs->lead) ?></div>
+			
+			<?php 
+				foreach($subs->resources as $resource)
+				{
+			?>
+				<span class="label label-info"><?php echo CHtml::encode($resource->name) ?></span>
+			<?php
+				}
+			?>
 	<?php
 		}
 	?>
