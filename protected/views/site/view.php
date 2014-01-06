70475ed8178fbe288d93db5af607bf2c5535a910
Jaydeep Patel <nisharg@gmail.com>
2014-01-06 16:06:30 +1100

    view ui changes

--- protected/views/site/view.php
+++ protected/views/site/view.php
@@ -1,26 +1,33 @@
 <div class="view">
-	<h1><?php echo CHtml::encode($model->theme) ?></h3>
-	<h6><?php echo date('d-m-Y', strtotime($model->s_date)) ?></h6>
-
-	<?php 
+	<div class="row-fluid heading">
+		<h1 class="title"><?php echo CHtml::encode($model->theme) ?></h3>
+		<span><i class="icon-calendar"></i><span id="date"><?php echo date('d-m-Y', strtotime($model->s_date)) ?></span></span>
+	</div>
+	<?php
 		foreach($model->sub_schedules as $subs)
 		{
 	?>
-			<h3><span><?php echo date('H:i', strtotime($subs->start_time)) ?></span> to <span><?php echo date('H:i', strtotime($subs->end_time)) ?></span> <?php echo CHtml::encode($subs->title) ?></h3>
-			<div>Lead: <?php echo CHtml::encode($subs->lead) ?></div>
-			<div>
-				<span>Duration: <?php echo date_diff(new DateTime($subs->start_time), new DateTime($subs->end_time))->format('%i mins') ?></span>
-			</div>
-			<div>Presenter: <?php echo CHtml::encode($subs->presenter) ?></div>
+		<div class="row-fluid schedule">
+			<span class="time-frame">
+				<div class="from"><?php echo date('H:i', strtotime($subs->start_time)) ?></div>
+				<div class="to"><?php echo date('H:i', strtotime($subs->end_time)) ?></div>
+			</span>
+			<span class="details">
+				<div class="sub-title"></span> <?php echo CHtml::encode($subs->title) ?></div>
+				<div class="duration"><i class="icon-time"></i> <?php echo date_diff(new DateTime($subs->start_time), new DateTime($subs->end_time))->format('%i mins') ?></div>
+				<div class="presenter"><i class="icon-user"></i> <?php echo CHtml::encode($subs->presenter) ?></div>
+				<div class="leaded-by">Leaded by <span><?php echo CHtml::encode($subs->lead) ?></span></div>
 
-			<?php 
-				foreach($subs->resources as $resource)
-				{
-			?>
-				<span class="label label-info"><?php echo CHtml::encode($resource->name) ?></span>
-			<?php
-				}
-			?>
+				<?php 
+					foreach($subs->resources as $resource)
+					{
+				?>
+					<span class="label label-info"><?php echo CHtml::encode($resource->name) ?></span>
+				<?php
+					}
+				?>
+			</span>
+		</div>
 	<?php
 		}
 	?>
