9df8a66ab0cdec6911d076679c1a965a64f1f182
Jaydeep <jaydeep@jdp.(none)>
2014-05-30 23:18:31 +1000

    Some more routes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('schedular-ui', [ 'ngRoute', 'restangular' ]);
+var app = angular.module('schedular-ui', [ 'ngRoute', 'restangular', 'ui.bootstrap' ]);
 
 app.config(function(RestangularProvider) {
   RestangularProvider.setBaseUrl('http://localhost:3000');
@@ -11,10 +11,20 @@ app.config(['$routeProvider', function($routeProvider) {
         controller: 'ScheduleListController',
         templateUrl: 'schedules.html'
       })
+    .when('/schedules/new',
+      {
+        controller: 'ScheduleNewController',
+        templateUrl: 'schedulesNew.html'
+      })
     .when('/schedules/:scheduleId',
       {
         controller: 'ScheduleDetailController',
         templateUrl: 'schedulesDetail.html'
+      })
+    .when('/schedules/:scheduleId/edit',
+      {
+        controller: 'ScheduleEditController',
+        templateUrl: 'schedulesEdit.html'
       })
     .otherwise(
       {
@@ -29,7 +39,7 @@ app.controller('ScheduleListController', ['$scope', '$location', 'Restangular', 
   });
 
   $scope.showDetail = function($schedule) {
-    $location.path('/schedules/' + $schedule.id);
+    $location.path('/schedules/' + $schedule.id + '/edit');
   }
 }]);
 
@@ -40,9 +50,27 @@ app.controller('ScheduleDetailController', ['$scope', '$routeParams', 'Restangul
   });
 }]);
 
-app.controller('SubSchedulesController', ['$scope', '$routeParams', 'Restangular', function($scope, $routeParams, Restangular) {
-  var sub_schedules = Restangular.all('schedules/sub_schedules');
-  sub_schedules.getList().then(function($sub_schedules) {
-    $scope.sub_schedules = $sub_schedules;
+app.controller('ScheduleNewController', ['$scope', '$location', 'Restangular', function($scope, $location, Restangular) {
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+  $scope.submit = function() {
+    Restangular.all('schedules').post($scope.schedule).then(function($schedule) {
+      console.log($schedule.id);
+      $location.path('/schedules/' + $schedule.id + '/edit');
+    }, function() {
+      console.log("Error");
+    });
+  };
+}]);
+
+app.controller('ScheduleEditController', ['$scope', '$routeParams', 'Restangular', function($scope, $routeParams, Restangular) {
+  var schedule = Restangular.one('schedules/' + $routeParams.scheduleId);
+  schedule.get().then(function($schedule) {
+    $scope.schedule = $schedule;
   });
 }]);
